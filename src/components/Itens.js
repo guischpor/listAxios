9ebb60d6067d676458de7f4e050f79f13b99578a
guischpor <guischpor1@live.com>
2018-12-30 19:38:53 -0200

    Update Itens.js

--- src/components/Itens.js
+++ src/components/Itens.js
@@ -13,7 +13,6 @@ export default class itens extends React.Component {
 render() {
     return (
         <View style={styles.item}>
-            
             <View style={styles.foto}>
                 <Image style={{ height: 100, width: 100 }}
                     source={{ uri: this.props.item.foto}}
